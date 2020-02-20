082fd56068224d3a8e55c036aa95cafb716ea12b
roosiu <roosiu@gmail.com>
2020-02-20 23:43:27 +0100

    do zrobienia wyswietlanie spinnera przy wpisach z zalacznikami

--- includes/header.php
+++ includes/header.php
@@ -25,19 +25,59 @@
 </head>
 <body>
 <!--preloader DO ZROBIENIA !!!!!!!!!!!!!!-->
-<div id="mdb-preloader" class="d-flex justify-content-center">
-    <div class="spinner-border" role="status">
-      <span class="sr-only">Ładowanie...</span>
-    </div>
-  </div>
+<div class="box-preloader">
+  <div class="loading"></div>
+</div>
+
+
   <script>
   $(window).on('load', function() {
-  $('#mdb-preloader').addClass('loaded');
+  $('.box-preloader').fadeOut();
 });
   </script>
+
   <style>
-  #mdb-preloader.loaded {
-  opacity: 0;
-  transition: .3s ease-in 1s;
+  .box-preloader{
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        background-color: white;
+        z-index: 9999;
+    }
+.loading {
+   position: absolute;
+   left: 50%;
+   top: 50%;
+   height:60px;
+   width:60px;
+   margin:0px auto;
+   -webkit-animation: rotation .6s infinite linear;
+   -moz-animation: rotation .6s infinite linear;
+   -o-animation: rotation .6s infinite linear;
+   animation: rotation .6s infinite linear;
+   border-left:6px solid rgba(0,174,239,.15);
+   border-right:6px solid rgba(0,174,239,.15);
+   border-bottom:6px solid rgba(0,174,239,.15);
+   border-top:6px solid rgba(0,174,239,.8);
+   border-radius:100%;
+}
+
+@-webkit-keyframes rotation {
+   from {-webkit-transform: rotate(0deg);}
+   to {-webkit-transform: rotate(359deg);}
+}
+@-moz-keyframes rotation {
+   from {-moz-transform: rotate(0deg);}
+   to {-moz-transform: rotate(359deg);}
+}
+@-o-keyframes rotation {
+   from {-o-transform: rotate(0deg);}
+   to {-o-transform: rotate(359deg);}
+}
+@keyframes rotation {
+   from {transform: rotate(0deg);}
+   to {transform: rotate(359deg);}
 }
   </style>
