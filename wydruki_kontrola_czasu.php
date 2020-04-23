bf0973c79c61d7939177283f8676500472b0433f
roosiu <roosiu@gmail.com>
2020-04-23 14:40:25 +0200

    do zrobienia to samo co poprzednio

--- wydruki_kontrola_czasu.php
+++ wydruki_kontrola_czasu.php
@@ -20,10 +20,10 @@ if ($user->check()) { // Tylko dla użytkowników zalogowanych
             <div class="col">
             </div>
             <div class="col text-center">
-            <form method="post" action="#"><i class="fa fa-print" aria-hidden="true"></i> <b>LISTA OBECNOŚCI</b> <br/><span class="small" id="naglowek_dziennik"></span><span class="small" id="naglowek_dziennik2"></span>
+            <form method="post" action="wydruki_kontrola_czasu.php"><i class="fa fa-print" aria-hidden="true"></i> <b>LISTA OBECNOŚCI</b> <br/><span class="small" id="naglowek_dziennik"></span><span class="small" id="naglowek_dziennik2"></span>
             </div>
             <div class="col">
-            <button type="button" id="przycisk_generuj" class="btn btn-dark btn-sm text-uppercase float-right dontprint m-3" ><i class="fa fa-magic" aria-hidden="true"></i> Generuj</button>
+            <button type="submit" id="przycisk_generuj" class="btn btn-dark btn-sm text-uppercase float-right dontprint m-3" ><i class="fa fa-magic" aria-hidden="true"></i> Generuj</button>
             <button type="button" id="przycisk_filtr_wyczysc" class="btn btn-dark btn-sm text-uppercase float-right dontprint m-3" ><i class="fa fa-eraser" aria-hidden="true"></i> Wyczyść </button>
 
             </div>
@@ -31,20 +31,8 @@ if ($user->check()) { // Tylko dla użytkowników zalogowanych
 <script>
 jQuery(function(){
 
-  function daysInMonth (month, year) {
-    return new Date(year, month, 0).getDate();
-}
 
-  $("#nr_kol").on("change", function() {
 
-    kto_przypis = $(this).find("option:selected").val();
-    $("#pracownik").val(kto_przypis).change();
-
-  });
-  $("#pracownik").on("change", function() {
-    $("#nr_kol").find("option:selected").val($("#pracownik").find("option:selected").text());
-
-  });
 
   jQuery("#przycisk_filtr_wyczysc").click(function () {
     $(":input").val("");
@@ -53,111 +41,50 @@ jQuery(function(){
         $(".Editor-editor").html(data)
       });
     });
-  });
+  });';
+  if ($_POST) {
 
-  jQuery("#przycisk_generuj").click(function () {
-
-		for (x=1;x<=31;x++) {
-
-			$(".dzien_"+x).html(" ");
-
-
-		}
-    $("#jednos_wart").html($("#jednos").val());
-    $("#mies_rok").html("m-c:"+$("#miesiac").val()+" rok:"+$("#rok").val());
-
-    for (i = 1; i < 11; i++) {
-      if($.isNumeric($("#nr_kol option:contains("+i+")").val())) { } else {
-
-      $("#nazwisko_"+i).html($("#nr_kol option:contains("+i+")").val());
-      }
-    }
-
-
-    if ($("#dni_nie").checked = $("#dni_nie").is(":checked")) {
-			var rokj = $("#rok").val();
-			var miesiacj = $("#miesiac").val();
-			var dni_mies = daysInMonth(miesiacj,rokj);
-        for (y=(dni_mies+1);y<=31;y++) {
-          $(".dzien_"+y).html("XXXXX");
-
-        }
-    }
-
-		else
-		{
-
-    }
-
-
-    jQuery(function(e) {
-        if ( $("#sob_niedz").checked =  $("#sob_niedz").is(":checked")) {
-				var d = new Date();
-
-				var miesiacj = $("#miesiac").val();
-				var rokj = $("#rok").val();
-
-
-var d = new Date();
-var getTot = daysInMonth(d.getMonth(),d.getFullYear());
-var sat = new Array();
-var sun = new Array();
-
-for(var i=1;i<=getTot;i++){
-  var newDate = new Date(rokj,miesiacj-1,i)
-
-  if(newDate.getDay()==0){
-      sat.push(i)
-      $(".dzien_"+(i)).html("----------");
+    $pracownik_1 = (htmlspecialchars(trim($_POST['pracownik_1'])));
+    echo 'alert("'.$pracownik_1.'");';
+    echo '$("#pole_nazwisko_1").html("'.$pracownik_1.'");';
   }
-  if(newDate.getDay()==6){
-      sun.push(i)
-      $(".dzien_"+(i)).html("----------");
-  }
-}
 
 
-
-
-		}
-		else
-		{
-
-
-
-
-
-
-
-
-		}
-
-    });
-
-
-  });
+echo '
 });
 </script>
 <div id="div_filtr" class="row mb-3 dontprint justify-content-center">';
-$filtr_0_rozmiar = '-1';
-$filtr_0_text = 'NR KOL';
-$filtr_0 = '<select name="nr_kol" id="nr_kol" class="form-control form-control-sm">
-<option>1</option>
-<option>2</option>
-<option>3</option>
-<option>4</option>
-<option>5</option>
-<option>6</option>
-<option>7</option>
-</select>';
-$filtr_1_rozmiar = '-';
-$filtr_1_text = 'PRACOWNIK';
-$filtr_1 = '<select name="pracownik" id="pracownik" class="form-control form-control-sm"><option></option>'.tabeladb2('1','SELECT * FROM pracownicy ORDER BY `id` ASC', '', '', "<option value='", "1", "'>", "", "1", "</option>", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "").'</select>
-
+$filtr_0_rozmiar = '- my-2';
+$filtr_0_text = 'PRACOWNIK nr 1';
+$filtr_0 = '<select name="pracownik_1" id="pracownik_1" class="form-control form-control-sm"><option></option>'.tabeladb2('1','SELECT * FROM pracownicy ORDER BY `id` ASC', '', '', "<option value='", "1", "'>", "", "1", "</option>", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "").'</select>
 ';
-$filtr_2_rozmiar = '-2';
-$filtr_2_text = 'MIESIĄC';
-$filtr_2 = '<select name="miesiac" id="miesiac" class="form-control form-control-sm">
+$filtr_1_rozmiar = '- my-2';
+$filtr_1_text = 'PRACOWNIK nr 2';
+$filtr_1 = '<select name="pracownik_2" id="pracownik_2" class="form-control form-control-sm"><option></option>'.tabeladb2('1','SELECT * FROM pracownicy ORDER BY `id` ASC', '', '', "<option value='", "1", "'>", "", "1", "</option>", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "").'</select>
+';
+$filtr_2_rozmiar = '- my-2';
+$filtr_2_text = 'PRACOWNIK nr 3';
+$filtr_2 = '<select name="pracownik_3" id="pracownik_3" class="form-control form-control-sm"><option></option>'.tabeladb2('1','SELECT * FROM pracownicy ORDER BY `id` ASC', '', '', "<option value='", "1", "'>", "", "1", "</option>", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "").'</select>
+';
+$filtr_3_rozmiar = '- my-2';
+$filtr_3_text = 'PRACOWNIK nr 4';
+$filtr_3 = '<select name="pracownik_4" id="pracownik_4" class="form-control form-control-sm"><option></option>'.tabeladb2('1','SELECT * FROM pracownicy ORDER BY `id` ASC', '', '', "<option value='", "1", "'>", "", "1", "</option>", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "").'</select>
+';
+$filtr_4_rozmiar = '- my-2';
+$filtr_4_text = 'PRACOWNIK nr 5';
+$filtr_4 = '<select name="pracownik_5" id="pracownik_5" class="form-control form-control-sm"><option></option>'.tabeladb2('1','SELECT * FROM pracownicy ORDER BY `id` ASC', '', '', "<option value='", "1", "'>", "", "1", "</option>", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "").'</select>
+';
+$filtr_5_rozmiar = '- my-2';
+$filtr_5_text = 'PRACOWNIK nr 6';
+$filtr_5 = '<select name="pracownik_6" id="pracownik_6" class="form-control form-control-sm"><option></option>'.tabeladb2('1','SELECT * FROM pracownicy ORDER BY `id` ASC', '', '', "<option value='", "1", "'>", "", "1", "</option>", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "").'</select>
+';
+$filtr_6_rozmiar = '- my-2';
+$filtr_6_text = 'PRACOWNIK nr 7';
+$filtr_6 = '<select name="pracownik_7" id="pracownik_7" class="form-control form-control-sm"><option></option>'.tabeladb2('1','SELECT * FROM pracownicy ORDER BY `id` ASC', '', '', "<option value='", "1", "'>", "", "1", "</option>", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "").'</select>
+';
+$filtr_7_rozmiar = '-2 my-2';
+$filtr_7_text = 'MIESIĄC';
+$filtr_7 = '<select name="miesiac" id="miesiac" class="form-control form-control-sm">
 <option value = '.(date('m')).'>'.(date('m')).'</option>
 <option></option>
 <option value=01>01</option>
@@ -173,9 +100,9 @@ $filtr_2 = '<select name="miesiac" id="miesiac" class="form-control form-control
 <option value=11>11</option>
 <option value=12>12</option>
 </select>';
-$filtr_3_rozmiar = '-2';
-$filtr_3_text = 'ROK';
-$filtr_3 = '<select name ="rok" id="rok" class="form-control form-control-sm">
+$filtr_8_rozmiar = '-2 my-2';
+$filtr_8_text = 'ROK';
+$filtr_8 = '<select name ="rok" id="rok" class="form-control form-control-sm">
 <option value = '.(date('Y')).'>'.(date('Y')).'</option>
 <option></option>
 <option value = '.(date('Y', strtotime(" +1 year"))).'>'.(date('Y', strtotime(" +1 year"))).'</option>
@@ -183,15 +110,17 @@ $filtr_3 = '<select name ="rok" id="rok" class="form-control form-control-sm">
 <option value = '.(date('Y', strtotime(" -2 year"))).'>'.(date('Y', strtotime(" -2 year"))).'</option>
 <option value = '.(date('Y', strtotime(" -1 year"))).'>'.(date('Y', strtotime(" -1 year"))).'</option>
 </select>';
-$filtr_4_rozmiar = '-3';
-$filtr_4_text = 'JEDNOSTKA ORGANIZACYJNA';
-$filtr_4 = '<input type="text" class="form-control form-control-sm" id="jednos" name="jednos" value="
+$filtr_9_rozmiar = '-3 my-2';
+$filtr_9_text = 'JEDNOSTKA ORGANIZACYJNA';
+$filtr_9 = '<input type="text" class="form-control form-control-sm" id="jednos" name="jednos" value="
 '.pojed_zapyt('SELECT nazwa_jednostki FROM ustawienia_ogolne WHERE `id` = 1').'">';
 
 
 
-for ($x = 0; $x <= 4; $x++) {
-
+for ($x = 0; $x <= 9; $x++) {
+if($x==7){
+  echo '</div><div id="div_filtr_2" class="row mb-3 dontprint justify-content-center">';
+}
   echo ' <div class="col-sm'.${filtr_.$x._rozmiar}.' px-1">
   <div class="card bg-light">
   <div class="card-body p-2">
